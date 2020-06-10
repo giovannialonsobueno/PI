e3242530ff51ff57fc35f81ed3de86f021e461f4
Celso Matiazi <62525198+CelsoMatiazi@users.noreply.github.com>
2020-06-10 13:49:33 -0300

    atualização no footer
    anexando o arquivo do footer modificado nos arquivos.

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,8 @@
     <h3>conheça</h3>
   <a href="https://theuselessweb.com/" target="_blank">Quem somos</a>
   <a href="https://theuselessweb.com/" target="_blank">Trabalhe conosco</a>
+  <a href="https://theuselessweb.com/" target="_blank">Polica de Privacidade</a>
+  <a href="https://theuselessweb.com/" target="_blank">Empresa</a>
   <a href="https://theuselessweb.com/" target="_blank"></a>
   </div>
 
@@ -10,11 +12,16 @@
     <h3>viagens</h3>
   <a href="https://theuselessweb.com/" target="_blank">Hotéis</a>
   <a href="https://theuselessweb.com/" target="_blank">Passagens aéreas</a>
+  <a href="https://theuselessweb.com/" target="_blank">Pacotes</a>
+  <a href="https://theuselessweb.com/" target="_blank">Promoções</a>
+  <a href="https://theuselessweb.com/" target="_blank">Corporativos</a>
   </div>
 
   <div class="footer_info">
     <h3>fale conosco</h3>
     <a href="https://theuselessweb.com/" target="_blank">FAQ</a>
+    <a href="https://theuselessweb.com/" target="_blank">Cadastre sua empresa</a>
+    <a href="https://theuselessweb.com/" target="_blank">SAC</a>
     <a href="https://theuselessweb.com/" target="_blank"></a>
   </div>
 
