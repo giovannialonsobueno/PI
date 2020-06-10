7edfc488030e3df523e9b803ce27469a7363a401
Celso Matiazi <62525198+CelsoMatiazi@users.noreply.github.com>
2020-06-10 13:33:48 -0300

    Novo: index, header, footer
    Atualização na pagina Home, e o header.
    modificação drastica no layout. (Re-work)

--- index.php
+++ index.php
@@ -2,16 +2,20 @@
 <html lang="en" dir="ltr">
 
 <head>
-  
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/home2.css">
+  <link rel="stylesheet" href="css/header3.css">
   <script src="https://kit.fontawesome.com/837f81fff7.js" crossorigin="anonymous"></script>
   <title>Welcome - Home Page </title>
 </head>
 <body>
-  <?php include("includes/header.php"); ?>
-  <img class="banner" src="img/banner1.jpg" alt="">
+  <?php include("includes/header3.php"); ?>
+  <video width="100.03%" height="100%" autoplay loop>
+    <source src="img/estaVago.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+  </video>
     <h2 class="titulo-promocao">As melhores promoções para você</h2>
 
       <section class="grid-main">
@@ -116,7 +120,6 @@
       </div>
   </section>
 
-<img class="banner" src="img/banner2.jpg" alt="">
       <section class="grid-main">
       <div class="div"> <img src="destinos/CastiloDeColomares-Espanha.jpg" alt="">
         <div class="info-bloco">
@@ -219,6 +222,53 @@
       </div>
   </section>
 
+<h2>Conheça o Brasil</h2>
+
+<section class="brasil">
+  <div class="card-brasil" style="background-image: url('destinos/brasil1.jpg');">
+    <button type="button" name="button" style="background-color: #0ca879;">Fernando de Noronha</button>
+  </div>
+
+  <div class="card-brasil" style="background-image: url('destinos/brasil2.jpg');">
+    <button type="button" name="button" style="background-color: #edbf18;">Florianopolis</button>
+  </div>
+
+  <div class="card-brasil" style="background-image: url('destinos/brasil3.jpg');">
+    <button type="button" name="button" style="background-color: #2e5cbf;">Minas Gerais</button>
+  </div>
+
+  <div class="card-brasil" style="background-image: url('destinos/brasil5.jpg');">
+    <button type="button" name="button" style="background-color: #b336a6;">Mato Grosso</button>
+  </div>
+</section>
+
+<section class="box-info">
+  <div class="card-info">
+    <img src="icon-img/calendario2.png" alt="">
+      <div class="box-texto">
+        <h2>Cancelamento gratuito.</h2>
+        <p>Cancele sua reserva com de forma rapida e facil.</p>
+
+      </div>
+  </div>
+
+  <div class="card-info">
+    <img src="icon-img/aviao.png" alt="">
+    <div class="box-texto">
+    <h2>Melhores opções</h2>
+    <p>As melhores opções de destino para você e sua familia</p>
+    </div>
+  </div>
+
+  <div class="card-info">
+    <img src="icon-img/cadeado.png" alt="">
+    <div class="box-texto">
+    <h2>Segurança</h2>
+    <p>Segurança e privacidade para as suas compras.</p>
+    </div>
+  </div>
+</section>
+
 <h2>Como foi a sua viagem?</h2>
 
   <section class="coments">
@@ -229,7 +279,11 @@
           <hr>
           <div class="comment-area">
           <span class="horario">1 semana atrás</span><br>
-          <span class="comentario">Ótima vista para o mar, a parte da piscina ótima estrutura, os quartos são aconchegante, não está tão em conta a diária como era com os antigos gestores, mas vale a pena ir pelo menos uma vez na vida rsrsrsrs.. </span>
+          <span class="comentario">Atendimento atencioso e solicitos de todos staff do hotel.
+                                    Excelente café da manhã com produtos de qualidades e muita variedade.
+                                    Quartos impecavelmente limpos.
+                                    Ótima localização.
+                                    Excelente ambiente digno de um hotel 58. </span>
         </div>
       </div>
     </div>
@@ -241,7 +295,9 @@
           <hr>
           <div class="comment-area">
           <span class="horario">2 semana atrás</span><br>
-          <span class="comentario">Ótima vista para o mar, a parte da piscina ótima estrutura, os quartos são aconchegante, não está tão em conta a diária como era com os antigos gestores, mas vale a pena ir pelo menos uma vez Ótima vista para o mar, a parte da piscina ótima estrutura, os quartos são aconchegante, não está tão em conta a diária como era com os antigos gestores, mas vale a pena ir pelo menos uma vez  na vida rsrsrsrs.. </span>
+          <span class="comentario">With spacious, modern and renovated rooms, this hotel is a classic in Rio de Janeiro. Arguably and the most elegant and traditional, with an excellent restaurant infrastructure and good leisure area, this hotel is a necessary experience for those who wish to enjoy the luxury of a hotel with a beach city in Brazil.
+
+                                    Com quartos espaços, modernos e reformados, este hotel é um clássico do Rio de Janeiro. Indiscutivelmente e o mais elegante e tradicional, com uma excelente infraestrutura de restaurantes e boa área de lazer, tonam esse hotel uma experiencia necessária para aqueles que desejam usufruir o luxo de um hotel com uma cidade de praia do Brasil. </span>
         </div>
       </div>
     </div>
@@ -253,7 +309,7 @@
           <hr>
           <div class="comment-area">
           <span class="horario">2 meses atrás</span><br>
-          <span class="comentario">Ótima vista para o mar, a parte da piscina ótima estrutura, os quartos são aconchegante, não está tão em conta a diária como era com os antigos gestores, mas vale a pena ir pelo menos uma vez na vida rsrsrsrs.. </span>
+          <span class="comentario">Estivemos em um final de semana com a familia - 6 apartamentos, e não posso deixar de agradecer o carrinho e a atenção do maitre Santos e do Gonçalves. Feijoada com roda de samba fantástica, atendimento sempre nota 10. A torta de Brie com damascos do Brunch mantém o padão....e os drinks na piscina são sempre maravilhosos. Parabéns!!! </span>
         </div>
       </div>
     </div>
