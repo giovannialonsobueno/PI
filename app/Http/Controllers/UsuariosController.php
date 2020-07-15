e794467ce9799d81c330aa4b3eb5f25d32e3d477
VInicius-Mance <vinimanceprof@gmail.com>
2020-07-15 11:50:50 -0300

    correções e ajustes
    correções de css e validações de cadastro

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -10,28 +10,10 @@ class UsuariosController extends Controller
 {
   function cadastro(Request $request)
   {
-    $request->nomeOK = true;
-    $request->emailOK = true;
-    $request->senhaOK = true;
+      // $validacoes = $request->validate([
+      //   'email' => 'required|email'
+      // ]);
 
-    if (strlen($request->nome) >= 6)
-    {
-      $request->nomeOK = false;
-    }
-
-    if (strlen($request->email) >= 6)
-    {
-      $request->emailOK = false;
-    }
-
-    if (strlen($request->senha) >= 6 and
-    $request->senha == $request->confirmar)
-    {
-      $request->senhaOK = false;
-    }
-    if (!$request->nomeOK and !$request->emailOK
-    and !$request->senhaOK)
-    {
       $user = new User();
       $user->nome  = $request->nome;
       $user->email = $request->email;
@@ -40,23 +22,20 @@ class UsuariosController extends Controller
       $email = $request->email;
       $senha = $request->senha;
       if (Auth::attempt([ "email"=>$email, "password"=>$senha ]))
-      { return redirect('/'); } else {
-        return redirect('/entrar');
-      }
-    }
+      { return redirect('/'); }
+      else { return redirect('/entrar'); }
+  }
 
-    }
     function login(Request $request){
         $email = $request->email;
         $senha = $request->senha;
         if (Auth::attempt([ "email"=>$email, "password"=>$senha ]))
-        { return redirect('/'); } else {
-          return redirect('/entrar');
-        }
+        { return redirect('/'); }
+        else { return redirect('/entrar');}
     }
 
-      function logout(Request $request){
+    function logout(Request $request){
       Auth::logout();
       return redirect('/');
-      }
+    }
 }
